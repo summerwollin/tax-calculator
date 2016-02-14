dbce96f1ba60dd53c1adcc48482b83d29981f397
Summer Wollin <summer.wollin@gmail.com>
2016-02-14 12:16:43 -0700

    Add first two tests to test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
 var code = require('../main');
 var expect = require('chai').expect;
 
-describe('', function() {
-  it('');
+describe('Tax Calculator', function(){
+
+  it('should tax 10% on the first $10', function() {
+    expect(code.calcTax(1)).to.equal(0.1);
+    expect(code.calcTax(10)).to.equal(1);
+  });
+
+  it('should tax 7% on the second $10', function(){
+    expect(code.calcTax(15)).to.equal(1.35);
+  });
+
 });
