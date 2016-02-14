e28cd5c5c1a9e71c8c5ae1e50352d154811ddebb
Summer Wollin <summer.wollin@gmail.com>
2016-02-14 13:59:42 -0700

    Refractor code, include more test cases, all passing

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 module.exports = {
   calcTax: function(num) {
     if (num <= 10) {return num * 0.1}
-    if (num > 10 && num < 20) {
-      return (((num - 10) * 0.07) + 1);
-    }
-    if (num > 20 && num < 30) {
-      return ((num - 20) * 0.05) + 1.7;
-    }
-    else {
-      return ((num - 30) * 0.03) + 2.2;
-    }
+    if (num > 10 && num < 20) {return (((num - 10) * 0.07) + 1)}
+    if (num > 20 && num < 30) {return ((num - 20) * 0.05) + 1.7}
+    else {return ((num - 30) * 0.03) + 2.2}
   }
 }
--- test/test.js
+++ test/test.js
@@ -8,11 +8,15 @@ describe('calcTax', function(){
   })
   it('should tax 7% on the second $10', function(){
     expect(code.calcTax(15)).to.equal(1.35);
+    expect(code.calcTax(17)).to.equal(1.49);
+    expect(code.calcTax(10.01)).to.equal(1.0007);
   })
   it('should tax 5% on the third $10', function(){
     expect(code.calcTax(25)).to.equal(1.95);
+    expect(code.calcTax(30)).to.equal(2.2);
   })
   it('should tax 3% on everything after the third $10', function(){
     expect(code.calcTax(35)).to.equal(2.35);
+    expect(code.calcTax(48)).to.equal(2.74);
   })
 });
